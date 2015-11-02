6d0e4561ccffd562661e30de635a31da11a7a028
steflocco <user01@user01-PC>
2015-11-02 14:28:27 +0100

    l

--- src/pappaebuffa/controller/azioni/Login.java
+++ src/pappaebuffa/controller/azioni/Login.java
@@ -2,14 +2,10 @@ package pappaebuffa.controller.azioni;
 
 import javax.servlet.http.HttpServletRequest;
 
-import pappaebuffa.controller.azioni.Azione;
-import pappaebuffa.controller.bean.Errore;
 import pappaebuffa.controller.form.Form;
 import pappaebuffa.controller.form.LoginForm;
 import pappaebuffa.model.dao.DAOCliente;
 import pappaebuffa.model.dao.eccezioni.DAOException;
-import pappaebuffa.model.dao.eccezioni.DAOLoginException;
-
 
 public class Login implements Azione {
 
