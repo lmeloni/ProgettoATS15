87dfdcc1e8c97b57efb467fea0d68403c3608051
steflocco <user01@user01-PC>
2015-11-03 17:21:39 +0100

    MostraOrdiniRistorante....

--- src/pappaebuffa/controller/azioni/MostraOrdinePerRistorante.java
+++ src/pappaebuffa/controller/azioni/MostraOrdinePerRistorante.java
@@ -17,19 +17,19 @@ public class MostraOrdinePerRistorante implements Azione{
 	@Override
 	public String esegui(HttpServletRequest request, Form form) {
 		
+		//(Ristorante)request.getSession().getAttribute("utente")); //???
+		Ristorante ristorante = new Ristorante(1,null,null,null,null,null,null,null,null,null,null);
 		try {
-//		MostraOrdinePerRistoranteForm f=(MostraOrdinePerRistoranteForm) form;
+			DAOOrdine dao = new DAOOrdine();
+	
+			request.setAttribute("ordiniRistorante", dao.selectByRistorante(ristorante));
+	
+			return "showOrdiniRistorante.jsp";
 		
-		DAOOrdine dao = new DAOOrdine();
-		ArrayList<Ordine> arraylistOrdine = dao.selectByRistorante(new Ristorante(1,null,null,null,null,null,null,null,null,null,null));//(Ristorante)request.getSession().getAttribute("utente"));
-
-		request.setAttribute("ordiniRistorante", arraylistOrdine);
-
-		return "showOrdiniRistorante.jsp";
-	} catch (DAOException e) {
-		e.printStackTrace();
-		return "errore.jsp"; // VERIFICARE SE VA BENE
-	}
+		} catch (DAOException e) {
+			request.setAttribute("errore", e.getMessage());
+			return "errore.jsp"; // VERIFICARE SE VA BENE
+		}
 
 	}
 
