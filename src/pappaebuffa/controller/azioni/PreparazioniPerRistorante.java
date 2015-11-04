338eb80b348259b10254fb8760808ccba72f1da2
user11 <user11@user11-PC>
2015-11-04 12:22:51 +0100

    Rimuovi dalla tabella preparazione  la pietanza tramite lo user id ristorante

--- src/pappaebuffa/controller/azioni/PreparazioniPerRistorante.java
+++ src/pappaebuffa/controller/azioni/PreparazioniPerRistorante.java
@@ -10,7 +10,7 @@ import pappaebuffa.model.dao.eccezioni.DAOConnessioneException;
 import pappaebuffa.model.dao.eccezioni.DAOException;
 import pappaebuffa.model.entity.Pietanza;
 
-import pappaebuffa.model.entity.Ristorante;;
+import pappaebuffa.model.entity.Ristorante;
 
 public class PreparazioniPerRistorante implements Azione {
 
