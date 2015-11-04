941e981a6311b32ed8a6d1d4d5e378a640ae7bf6
user10 <user10@user10-PC>
2015-11-04 11:12:26 +0100

    registrazione ristorante

--- src/pappaebuffa/controller/azioni/RegistrazioneRistorante.java
+++ src/pappaebuffa/controller/azioni/RegistrazioneRistorante.java
@@ -4,24 +4,36 @@ import javax.servlet.http.HttpServletRequest;
 
 import pappaebuffa.controller.form.Form;
 import pappaebuffa.controller.form.RegistrazioneForm;
+import pappaebuffa.controller.form.RegistrazioneRistoranteForm;
+import pappaebuffa.model.dao.DAO;
+import pappaebuffa.model.dao.DAORistorante;
+import pappaebuffa.model.dao.eccezioni.DAOConnessioneException;
+import pappaebuffa.model.dao.eccezioni.DAOException;
 import pappaebuffa.model.entity.Ristorante;
 
 public class RegistrazioneRistorante implements Azione {
 
 	@Override
 	public String esegui(HttpServletRequest request, Form form) {
+		// recupera i dati dal form
+		
+		RegistrazioneRistoranteForm r = (RegistrazioneRistoranteForm) form;
+		Ristorante ristorante = new Ristorante(0, r.getEmail(), r.getPassword(), r.getNome(), 
+				   r.getCategoria(), r.getIndirizzo(), r.getCitta(), r.getTelefono(),
+				   r.getDescrizione(), r.getOrario_apertura(), r.getOrario_chiusura());
+		
+		DAO<Ristorante> dao;
+		try {
+			dao = new DAORistorante();
+			dao.insert(ristorante);
+			r.setFeedback("La tua registrazione è andata buon fine...Buon lavoro!");
+			return "login.jsp";
+		} catch (DAOException e) {
+			request.setAttribute("errore", e.getMessage());
+			return "errore.jsp";
+		}
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		Ristorante ristorante = new Ristorante(0, null, null, null, null
-				, null, null, null, null, null, null);
-		return null;
-	}
+	}		 
 
 }
